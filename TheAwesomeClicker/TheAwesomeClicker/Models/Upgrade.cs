490b3c41f097d22397488dcd956160e7663c617b
Unknown <jcarpenter@student.neumont.edu>
2018-08-07 18:28:39 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ TheAwesomeClicker/TheAwesomeClicker/Models/Upgrade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAwesomeClicker.Models
+{
+    public class Upgrade
+    {
+        public Upgrade()
+        {
+
+        }
+
+        private int changeValue;
+
+        public int ChangeValue
+        {
+            get { return changeValue; }
+            set { changeValue = value; }
+        }
+
+        private bool isBought;
+
+        public bool IsBought
+        {
+            get { return isBought; }
+            set { isBought = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string iconPath;
+
+        public string IconPath
+        {
+            get { return iconPath; }
+            set { iconPath = value; }
+        }
+
+        private double cost;
+
+        public double Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+    }
+}
