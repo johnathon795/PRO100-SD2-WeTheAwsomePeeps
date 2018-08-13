1aaca6925495f89152e3d570d55949b74fdc13f1
Unknown <walkerdp357@gmail.com>
2018-08-13 16:38:42 -0600

    Yote audio into project

--- TheAwesomeClicker/TheAwesomeClicker/MainPage.xaml.cs
+++ TheAwesomeClicker/TheAwesomeClicker/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -14,23 +15,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.Playback;
+using Windows.Media.Core;
 
 namespace TheAwesomeClicker
 {
     public sealed partial class MainPage : Page
     {
         Game game;
+        MediaPlayer mp = new MediaPlayer
+        {
+            Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/Theme.wav")),
+            AudioCategory = MediaPlayerAudioCategory.GameMedia,
+            Volume = 1,
+            IsLoopingEnabled = true
+        };
         public MainPage()
         {
-            
+
             this.InitializeComponent();
             game = new Game()
             {
-                
+
             };
             upgradesListBox.ItemsSource = game.UpgradesList;
+            //game.PropertyChanged += Update_Coin;
             foreach (Upgrade up in game.UpgradesList) up.Tapped += Up_Tapped;
+
+            mp.Play();
+
         }
+
+        //private void Update_Coin(object sender, PropertyChangedEventArgs e)
+        //{
+        //    //if (e.PropertyName.Equals("TotalCoin"))
+        //    //{
+        //        //pointsBox.Text = game.TotalCoin.ToString();
+        //    //}
+        //}
 
         private void Up_Tapped(object sender, TappedRoutedEventArgs e)
         {
